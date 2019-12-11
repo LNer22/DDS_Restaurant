c47810d1f014e9fcecdce163fb72779853797351
Lener Quiroz <quirozlener@gmail.com>
2019-12-11 11:22:10 -0600

    something

--- DDS_Restaurant_Solution/DDS_Restaurant_Solution/Forms/frmMesasOrdenes.cs
+++ DDS_Restaurant_Solution/DDS_Restaurant_Solution/Forms/frmMesasOrdenes.cs
@@ -19,9 +19,18 @@ namespace DDS_Restaurant_Solution.Forms
         
         private void frmMesasOrdenes_Load(object sender, EventArgs e)
         {
-
+            Button btn = new Button()
+            {
+                BackColor = System.Drawing.Color.Transparent,
+                FlatStyle = System.Windows.Forms.FlatStyle.Flat,
+                Font = new System.Drawing.Font("Lucida Console", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0),
+                ForeColor = System.Drawing.Color.Black,
+                Size = new System.Drawing.Size(180, 50),
+                UseVisualStyleBackColor = false,
+                Tag = "",
+                Text = $""
+            };
+            //btn.Click += btnMenu_Click;
         }
-        
-        
     }
 }
